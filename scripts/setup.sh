21989346e419d23662461ca9035c0188bdd4df26
Tino Weiss <tino.weiss@pointsource.com>
2017-07-11 11:54:30 -0400

    Update build scripts
    Updated the build scripts to remove client secret field (no longer used)
    and add domain, service account email, service account key file path,
    and app account email fields used for Google directory user list.
    
    Resolves #24

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,13 +9,16 @@ WORKSPACE=`pwd`
 read -p 'Enter the API host > ' API
 read -p 'Enter allowed origins > ' ALLOWED_ORIGINS
 read -p 'Enter the Google client ID > ' GOOGLE_CLIENT_ID
-read -p 'Enter the Google secret > ' GOOGLE_CLIENT_SECRET
 read -p 'Enter the Mongo DB host > ' MONGODB_HOST
 read -p 'Enter the Mongo DB port > ' MONGODB_PORT
 read -p 'Enter the Mongo DB database > ' MONGODB_DB
 read -p 'Enter the Mongo DB username > ' MONGODB_USERNAME
 read -p 'Enter the Mongo DB password > ' MONGODB_PASSWORD
 read -p 'Enter the decryption password > ' DECRYPTION_PASSWORD
+read -p 'Enter the Google domain > ' GOOGLE_DOMAIN
+read -p 'Enter the service account email > ' SERVICE_ACCOUNT_EMAIL
+read -p 'Enter the service account key file path > ' SERVICE_ACCOUNT_KEY_FILE_PATH
+read -p 'Enter the app account email > ' APP_ACCOUNT_EMAIL
 
 echo
 
