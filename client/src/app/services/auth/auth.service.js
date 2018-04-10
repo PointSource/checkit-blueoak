d23d99cba688f634716b3764f0ca9264625f1dd4
Tino Weiss <tino.weiss@pointsource.com>
2018-04-10 16:08:59 -0400

    Fix issue where access token was set too late

--- client/src/app/services/auth/auth.service.js
+++ client/src/app/services/auth/auth.service.js
@@ -204,8 +204,8 @@
                 //startgoogle
                 if (method === 'google') {
                     GoogleService.login(appConfig.apiKeys).then(function(authResult) {
+                            GoogleUserService.setAccessToken(authResult.accessToken);
                             _loginSuccess(authResult.idToken).then(function() {
-                                GoogleUserService.setAccessToken(authResult.accessToken);
                                 deferred.resolve(authResult);
                                 setIsAuthenticated(true);
                             }, function(err) {
